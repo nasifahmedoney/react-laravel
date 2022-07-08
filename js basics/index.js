9bd42e6bfa332db339f08c816e5d26306f7eff0b
nasifahmedoney <nasif.ahmed@live.com>
2022-07-09 02:32:37 +0600

    arrow function

--- js basics/index.js
+++ js basics/index.js
@@ -141,6 +141,29 @@ console.log(sum(10));//output: 40
  */
 
 //arrow function with examples
+//codeacademy cheatsheet
+// Arrow function with two arguments 
+/* const sum = (firstParam, secondParam) => { 
+    return firstParam + secondParam; 
+  }; 
+  console.log(sum(2,5)); // Prints: 7 
+   
+  // Arrow function with no arguments 
+  const printHello = () => { 
+    console.log('hello'); 
+  }; 
+  printHello(); // Prints: hello
+   
+  // Arrow functions with a single argument 
+  const checkWeight = weight => { 
+    console.log(`Baggage weight : ${weight} kilograms.`); 
+  }; 
+  checkWeight(25); // Prints: Baggage weight : 25 kilograms.
+   
+   
+  // Concise arrow functions
+  const multiply = (a, b) => a * b; 
+  console.log(multiply(2, 30)); // Prints: 60  */
 /* single parameter dont use (),0 or 2+ parameter use (),
 single line function body doenst need "return" keyword.
 */ 
